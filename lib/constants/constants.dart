c0f5935a38b79e3c9f47e2dd1ed5e585d1efb4f2
Moatez Tilouche <133597264+MoatezTilouche@users.noreply.github.com>
2025-07-15 04:50:13 -0500

    Update constants.dart

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,4 +1,4 @@
-const apiKey = 'e7e9d3be2fd4de2754fb8700716a4c82';
+const apiKey = 'Your TMDB api key';
 const String baseUrl = 'https://api.themoviedb.org/3';
 
 const String geminiApiKey='Your geminy API KEY HERE';
